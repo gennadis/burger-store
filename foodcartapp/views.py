fc59cd37eb65a55b758a5c27f821b6ea6cd1749a
Gennady Badinov <gennady.badinov@gmail.com>
2022-05-04 22:38:00 +0300

    Refactor data validation logic with DRF Serializer

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -1,11 +1,10 @@
-from math import prod
-from django.db import IntegrityError
 from django.http import JsonResponse
 from django.templatetags.static import static
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-
+from rest_framework.serializers import ModelSerializer, IntegerField, CharField
+from phonenumber_field.serializerfields import PhoneNumberField
 
 from .models import Order, OrderProduct, Product
 
@@ -72,52 +71,52 @@ def product_list_api(request):
     )
 
 
+class OrderProductSerializer(ModelSerializer):
+    quantity = IntegerField(source="amount")
+
+    class Meta:
+        model = OrderProduct
+        fields = ["product", "quantity"]
+
+
+class OrderSerializer(ModelSerializer):
+    products = OrderProductSerializer(many=True, allow_empty=False)
+    firstname = CharField(source="first_name")
+    lastname = CharField(source="last_name")
+    phonenumber = PhoneNumberField(source="phone_number")
+
+    class Meta:
+        model = Order
+        fields = [
+            "firstname",
+            "lastname",
+            "phonenumber",
+            "address",
+            "products",
+        ]
+
+
 @api_view(["POST"])
 def register_order(request):
-    order_details = request.data
+    serializer = OrderSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
 
-    if (
-        not all(
-            isinstance(x, str)
-            for x in [
-                order_details.get("firstname"),
-                order_details.get("lastname"),
-                order_details.get("phonenumber"),
-                order_details.get("address"),
-            ]
-        )
-        or len(order_details.get("phonenumber")) == 0
-    ):
-        return Response(data="Bad order details!", status=status.HTTP_400_BAD_REQUEST)
+    order = Order.objects.create(
+        first_name=serializer.validated_data["first_name"],
+        last_name=serializer.validated_data["last_name"],
+        phone_number=serializer.validated_data["phone_number"],
+        address=serializer.validated_data["address"],
+    )
 
-    try:
-        order = Order.objects.create(
-            first_name=order_details["firstname"],
-            last_name=order_details["lastname"],
-            phone_number=order_details["phonenumber"],
-            address=order_details["address"],
-        )
-    except (
-        IntegrityError,
-        KeyError,
-    ):
-        return Response(data="Bad order details!", status=status.HTTP_400_BAD_REQUEST)
-
-    order_products = order_details.get("products")
-
-    if not isinstance(order_products, list) or not order_products:
-        return Response(data="Bad products!", status=status.HTTP_400_BAD_REQUEST)
+    order_products = serializer.validated_data["products"]
 
     for product in order_products:
-        try:
-            order_product = OrderProduct.objects.create(
-                order=order,
-                product=Product.objects.get(pk=product["product"]),
-                amount=product["quantity"],
-            )
-        except Product.DoesNotExist:
-            return Response(
-                data="Product does not exist!", status=status.HTTP_400_BAD_REQUEST
-            )
+        order_product = OrderProduct.objects.create(
+            order=order,
+            product=product["product"],
+            amount=product["amount"],
+        )
 
-    return Response(data="Order placed", status=status.HTTP_200_OK)
+    return Response(
+        data=f"Order {order.id} placed successfully", status=status.HTTP_200_OK
+    )
