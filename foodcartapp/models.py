42f28ffc48c02756fe675fffda8199a0ade41131
Gennady Badinov <gennady.badinov@gmail.com>
2022-05-13 23:31:27 +0300

    Get order suitable restaurants distances

--- foodcartapp/models.py
+++ foodcartapp/models.py
@@ -1,3 +1,6 @@
+import requests
+from geopy import distance
+from django.conf import settings
 from django.db import models
 from django.utils import timezone
 from django.core.validators import (
@@ -112,7 +115,6 @@ class OrderQuerySet(models.QuerySet):
     def with_total_prices(self):
         return self.annotate(total_price=Sum("products__static_price"))
 
-    # TODO refactor with annotate method
     def with_restaurants(self):
         for order in self:
             order_products = order.products.values("product")
@@ -134,7 +136,19 @@ class OrderQuerySet(models.QuerySet):
                 restaurants.append(restaurants_with_product)
 
             suitable_restaurants = restaurants[0].intersection(*restaurants)
-            order.suitable_restaurants = suitable_restaurants
+
+            suitable_restaurants_with_distances = []
+            for restaurant in suitable_restaurants:
+                distance = calculate_distance(
+                    order_address=order.address,
+                    restaurant_address=restaurant.address,
+                    apikey=settings.YANDEX_APIKEY,
+                )
+                suitable_restaurants_with_distances.append((restaurant, distance))
+
+            order.suitable_restaurants_with_distances = sorted(
+                suitable_restaurants_with_distances, key=lambda pair: pair[1]
+            )
 
         return self
 
@@ -258,3 +272,36 @@ class OrderProduct(models.Model):
 
     def __str__(self):
         return f"{self.product} - {self.amount}"
+
+
+def fetch_coordinates(apikey: str, address: str):
+    base_url = "https://geocode-maps.yandex.ru/1.x"
+    response = requests.get(
+        base_url,
+        params={
+            "geocode": address,
+            "apikey": apikey,
+            "format": "json",
+        },
+    )
+    response.raise_for_status()
+    found_places = response.json()["response"]["GeoObjectCollection"]["featureMember"]
+
+    if not found_places:
+        return None
+
+    most_relevant = found_places[0]
+    lon, lat = most_relevant["GeoObject"]["Point"]["pos"].split(" ")
+    return lat, lon
+
+
+def calculate_distance(
+    order_address: str,
+    restaurant_address: str,
+    apikey: str,
+) -> float:
+    order_coordinates = fetch_coordinates(apikey, order_address)
+    restaurant_coordinates = fetch_coordinates(apikey, restaurant_address)
+    order_distance = distance.distance(order_coordinates, restaurant_coordinates).km
+
+    return order_distance
