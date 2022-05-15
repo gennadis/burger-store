6287fcd70eb20a049c6cbe905e49accc49d11ec5
Gennady Badinov <gennady.badinov@gmail.com>
2022-05-15 20:39:17 +0300

    Create `Locations` for existing orders

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from foodcartapp.models import Restaurant
+from foodcartapp.models import Restaurant, Order
 from locations.models import Location
 from locations.geocoding import fetch_coordinates
 
@@ -14,4 +14,16 @@ def add_restaurants_locations():
         print(restaurant_location)
 
 
+def add_orders_locations():
+    for order in Order.objects.all():
+        latitude, longitude = fetch_coordinates(order.address)
+        order_location = Location.objects.get_or_create(
+            address=order.address,
+            latitude=latitude,
+            longitude=longitude,
+        )
+        print(order_location)
+
+
 add_restaurants_locations()
+add_orders_locations()
