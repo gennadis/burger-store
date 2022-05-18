20d42b3a6f1c341669ceef16c2673d17a3999f51
Gennady Badinov <gennady.badinov@gmail.com>
2022-05-18 17:59:40 +0300

    Remove redundant code

--- locations/geocoding.py
+++ locations/geocoding.py
@@ -24,25 +24,3 @@ def fetch_coordinates(address: str):
     most_relevant = found_places[0]
     lon, lat = most_relevant["GeoObject"]["Point"]["pos"].split(" ")
     return lat, lon
-
-
-def calculate_distance(
-    order_address: str,
-    restaurant_address: str,
-) -> float:
-    try:
-        order_coordinates = Location.objects.get(address=order_address)
-        restaurant_coordinates = Location.objects.get(address=restaurant_address)
-
-    except Location.DoesNotExist:
-        order_coordinates = fetch_coordinates(order_address)
-        restaurant_coordinates = fetch_coordinates(restaurant_address)
-        order_distance = distance.distance(order_coordinates, restaurant_coordinates).km
-        return order_distance
-
-    order_distance = distance.distance(
-        (order_coordinates.latitude, order_coordinates.longitude),
-        (restaurant_coordinates.latitude, restaurant_coordinates.longitude),
-    ).km
-
-    return order_distance
