41cd54aae9185fef0b414aa26d878ab2b510bf22
Gennady Badinov <gennady.badinov@gmail.com>
2022-06-01 21:09:37 +0300

    Collect static wihtout prompt

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 set -e
 
-echo 'Pulling code updates from GitHub...'
+echo '1. Pulling code updates from GitHub...'
 git pull
 
-echo 'Installing project requirements...'
+echo '2. Installing project requirements...'
 source venv/bin/activate
 pip install -r requirements.txt
 npm ci --dev
 
-echo 'Building frontend...'
+echo '3. Building frontend...'
 ./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
 
-echo 'Collecting static files...'
-python manage.py collectstatic
+echo '4. Collecting static files...'
+python manage.py collectstatic --noinput
 
-echo 'Applying migrations...'
+echo '5. Applying migrations...'
 python manage.py migrate
 
-echo 'Reloading systemd daemons'
+echo '6. Reloading systemd daemons...'
 sudo systemctl reload nginx
 sudo systemctl restart burger-store.service
 
-echo 'Deploy completed'
+echo '7. Deploy completed!'
