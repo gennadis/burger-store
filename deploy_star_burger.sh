272c77da412bef1c2b0060d38a7b58c4a73e714d
Gennady Badinov <gennady.badinov@gmail.com>
2022-06-01 20:59:20 +0300

    Improve auto deploy script

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -3,3 +3,23 @@ set -e
 
 echo 'Pulling code updates from GitHub...'
 git pull
+
+echo 'Installing project requirements...'
+source venv/bin/activate
+pip install -r requirements.txt
+npm ci --dev
+
+echo 'Building frontend...'
+./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
+
+echo 'Collecting static files...'
+python manage.py collectstatic
+
+echo 'Applying migrations...'
+python manage.py migrate
+
+echo 'Reloading systemd daemons'
+sudo systemctl reload nginx
+sudo systemctl restart burger-store.service
+
+echo 'Deploy completed'
